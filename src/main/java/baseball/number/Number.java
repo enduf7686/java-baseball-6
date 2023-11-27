ef1e50973c8b477939f46042205de3f2451032a9
enduf7686 <enduf768640@gmail.com>
2023-11-27 15:58:18 +0900

    feat(number): Number 클래스 구현

---
+++ src/main/java/baseball/number/Number.java
@@ -0,0 +1,40 @@
+package baseball.number;
+
+import java.util.List;
+
+public record Number(List<Integer> numbers) {
+
+    public Number {
+        validate(numbers);
+    }
+
+    private void validate(List<Integer> numbers) {
+        checkSize(numbers);
+        checkRange(numbers);
+        checkDuplication(numbers);
+    }
+
+    private void checkSize(List<Integer> numbers) {
+        if (numbers.size() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkRange(List<Integer> numbers) {
+        int count = (int) numbers.stream()
+                .filter(number -> number >= 1 && number <= 9)
+                .count();
+        if (count != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkDuplication(List<Integer> numbers) {
+        int count = (int) numbers.stream()
+                .distinct()
+                .count();
+        if (count != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
